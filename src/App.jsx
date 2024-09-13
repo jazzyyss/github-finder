084caaafd010e2e720db86391b47ed261662d567
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-09-13 14:19:20 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+    <div className="text-3xl font-bold underline">
+      App
+    </div>
+    <button class="btn">Hello daisyUI</button>  
+    </>
+  )
+}
+
+export default App
