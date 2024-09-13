ca4d6177ecbc0659f6750c4ee6608436f473f32e
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-09-13 16:29:03 -0400

    Added the users component to receive github user objects and view their names on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import {Routes, Route} from 'react-router-dom'
+
 import Footer from "./components/layout/Footer"
 import Navbar from "./components/layout/Navbar"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+
 
 const App = () => {
   return (
@@ -7,7 +13,12 @@ const App = () => {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
-          Content
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
         </main>
         <Footer />
       </div>
