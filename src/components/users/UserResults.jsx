8e4d4faf2585f5c954e99dfd985b2d57842b9da7
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-09-17 13:24:09 -0400

    added the github context to the project

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect, useContext } from "react"
 import Spinner from "../layout/Spinner"
 import UserItem from "./UserItem"
+import GithubContext from "../../context/github/GithubContext"
 
 const UserResults = () => {
 
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const {
+    users, loading, fetchUsers
+  } = useContext(GithubContext)
 
   useEffect(()=>{
     fetchUsers()
   },[])
-
-  const fetchUsers = async () =>{
-    const res = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`,{
-      headers:{
-        Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`
-      }})
-
-      const data = await res.json()
-      setUsers(data)
-      setLoading(false)
-  }
 
   if(!loading){
     return(
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom"
 
+import { GithubProvider } from './context/github/GithubContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <App />
+      <GithubProvider>
+        <App />
+      </GithubProvider>
     </Router>
   </StrictMode>,
 )
