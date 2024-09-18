fe53acfd899ba07580e9717023fab2edadcd1228
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-09-18 17:38:27 -0400

    added the alert context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom"
 
 import { GithubProvider } from './context/github/GithubContext.jsx'
+import { AlertProvider } from './context/alert/AlertContext.jsx'
+
 import App from './App.jsx'
 import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <GithubProvider>
-        <App />
+        <AlertProvider>
+          <App />
+        </AlertProvider>
       </GithubProvider>
     </Router>
   </StrictMode>,
